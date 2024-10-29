f3b27f1528edf15bfce7a9f41c7079798cdb2b09
Alex <arothenberg@14west.us>
2024-10-28 21:04:30 -0400

    Start basic inventory system

---
+++ Source/TheForgottenPath1/Public/Item.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Item.h"
+
+// Sets default values
+AItem::AItem()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	ItemName = "Not Defined";
+	ItemDescription = "Not Defined";
+	Quantity = 1;
+	Image = nullptr;
+	Value = 1;
+	// TODO extend this to add more properties, say if the item is a weapon, armor, etc
+}
+
+// Called when the game starts or when spawned
+void AItem::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void AItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
---
+++ Source/TheForgottenPath1/Public/Item.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Item.generated.h"
+
+UCLASS()
+class THEFORGOTTENPATH1_API AItem : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AItem();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	FString ItemName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	FString ItemDescription;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	int Quantity;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	UTexture2D *Image;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	int Value;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	UStaticMeshComponent *ItemMesh;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
